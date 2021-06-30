afebfd8a8b8f0ad3d602152e159678085108c23a
sunaif-adkar <86029415+sunaif-adkar@users.noreply.github.com>
2021-06-30 14:14:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ class Bot(Client):
         self.USER, self.USER_ID = await User().start()
         await self.USER.send_message(
             chat_id=usr_bot_me.username,
-            text="😬🤒🤒"
+            text="😅NDA DOH BOT WORKING lle allengil @sunaif_adkar pm vaa"
         )
 
     async def stop(self, *args):
